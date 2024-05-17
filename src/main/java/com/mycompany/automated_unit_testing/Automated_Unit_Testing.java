9484ff8f730717454fa7efe54adee0a631f5f1df
Refiloe05 <161421573+Refiloe05@users.noreply.github.com>
2024-05-17 11:43:59 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/automated_unit_testing/Automated_Unit_Testing.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.automated_unit_testing;
+
+/**
+ *
+ * @author refil
+ */
+public class Automated_Unit_Testing {
+
+    public static void main(String[] args) {
+       
+        /* 
+        Automated Unit Testing --> Github Actions
+        - Modify the pom.mxl file
+        - Create a Workflow called JavaTest using Github Actions
+        - The JDK Version included in the workflow will depend on the version you need to create the program
+        */
+    }
+}
